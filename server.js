f9382c6487d72cd00080818e203adef8a609b1ac
avazqudratovv-mit <avazqudratovv@gmail.com>
2026-05-23 12:47:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongodb.connect(
 
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = process.env.PORT || 3000;
+      let PORT = process.env.PORT || 3005;
       server.listen(PORT, function () {
         console.log(
           `The server is running successfully on http://localhost:${PORT}`,
